84422d91f912b1922e30c5153dbb29ac73cd3312
Luana Yasmim Lourenço <luanayasmim11@outlook.com>
2023-06-14 16:39:46 -0300

    atualizando url

--- src/http/index.ts
+++ src/http/index.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from 'axios';
 
 const clienteHttp: AxiosInstance = axios.create({
     //baseURL: "http://localhost:3000/"
-    baseURL:'https://my-json-server.typicode.com/luanayasmim/alura-tracker/db/'
+    baseURL:'https://my-json-server.typicode.com/luanayasmim/alura-tracker/'
 });
 
 export default clienteHttp;
